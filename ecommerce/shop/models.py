4d3bbe31a0595565e0cd894ac7e5a37594a9ad0e
audrius spiridavicius <audriusspiridavicius@gmail.com>
2024-03-11 07:56:32 +0200

    Product/ProductQuantity relationship updated

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -24,7 +24,7 @@ class ModelDate(models.Model):
    
 class ProductQuantity(ModelDate, models.Model):
      
-    product = models.OneToOneField("Product", on_delete=models.CASCADE, related_name='product_quantity')
+    product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name='product_quantity')
     
     quantity = models.IntegerField()
     
