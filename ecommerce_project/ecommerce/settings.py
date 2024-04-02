28be7b694f4ba482a4ccb5f0553f82b56f4707b4
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-04-02 13:19:55 +0300

    setting file update

--- ecommerce_project/ecommerce/settings.py
+++ ecommerce_project/ecommerce/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR,'templates'),],
-        'APP_DIRS': True,
+        # 'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -77,6 +77,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'loaders': [
+            'django.template.loaders.app_directories.Loader',
+            'django.template.loaders.filesystem.Loader',
+        ],
         },
     },
 ]
